3072567fecb40d9e2001e0fe2c22eccc27e0b368
Abhishek Silawat <absilawat@gmail.com>
2023-04-20 21:40:26 +0530

    ADDED: Searchbar component is added to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "./App.css";
 import axios from "axios";
 import UserTable from "./components/UserTable";
 import Form from "./components/Form";
+import Searchbar from "./components/Searchbar";
 
 function App() {
   const [userList, setUserList] = useState([]);
+  const [filterList, setFilterList] = useState([]);
   useEffect(() => {
     getData();
   }, []);
@@ -14,12 +16,20 @@ function App() {
     axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
       // console.log(res);
       setUserList(res.data);
+      setFilterList(res.data);
     });
   };
   return (
     <div className="App">
-      <Form userList={userList} setUserList={setUserList} />
-      <UserTable userList={userList} />
+      <Searchbar setFilterList={setFilterList} userList={userList} />
+      <div className="container">
+        <Form
+          userList={userList}
+          setFilterList={setFilterList}
+          setUserList={setUserList}
+        />
+        <UserTable filterList={filterList} />
+      </div>
     </div>
   );
 }
